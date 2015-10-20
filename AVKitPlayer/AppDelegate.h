046cd771127774a1dd395781acc629320117bbdc
Alumno ITESM Campus Toluca <Alumno@imac-de-proyecciones-3.local>
2015-10-20 17:53:34 -0500

    Initial Commit

---
+++ AVKitPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVKitPlayer
+//
+//  Created by Alumno ITESM Campus Toluca on 20/10/15.
+//  Copyright © 2015 Alumno ITESM Campus Toluca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVKitPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVKitPlayer
+//
+//  Created by Alumno ITESM Campus Toluca on 20/10/15.
+//  Copyright © 2015 Alumno ITESM Campus Toluca. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
